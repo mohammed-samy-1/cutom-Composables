0f4ee04822a42afdd197f28e1f4013774273ecdb
mo samy <medo.samy.0123@mail.com>
2023-05-29 22:14:38 +0300

    animation for card

--- app/src/main/java/com/devmo/customcomposecomponents/ui/theme/Card.kt
+++ app/src/main/java/com/devmo/customcomposecomponents/ui/theme/Card.kt
@@ -1,76 +1,120 @@
 package com.devmo.customcomposecomponents.ui.theme
 
 import android.widget.Toast
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.*
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlin.math.abs
 import com.devmo.customcomposecomponents.R
 import com.steliospapamichail.creditcardmasker.viewtransformations.CardNumberMask
-import java.util.IdentityHashMap
+import com.steliospapamichail.creditcardmasker.viewtransformations.ExpirationDateMask
 
-
+var numberInput by mutableStateOf(false)
+var dateInput by mutableStateOf(false)
+var nameInput by mutableStateOf(false)
+var CVVInput by mutableStateOf(false)
+var cardholderName by mutableStateOf("")
+var cardNumber by mutableStateOf("")
+var expirationDate by mutableStateOf("")
+var cvv by mutableStateOf("")
 fun Path.standardQuadFromTo(from: Offset, to: Offset) {
     quadraticBezierTo(
-        from.x,
-        from.y,
-        abs(from.x + to.x) / 2f,
-        abs(from.y + to.y) / 2f
+        from.x, from.y, abs(from.x + to.x) / 2f, abs(from.y + to.y) / 2f
     )
+}
+
+fun Modifier.animatedBorder(
+    strokeWith: Dp, animationDuration: Int, isVisible: Boolean = false
+) = composed {
+    if (isVisible) {
+        val gradient =
+            listOf(Color(0xFFDAA520), Color(0xFFFFDF7E), Color(0xFFB8860B), Color(0xFFFFD700))
+        val infiniteTransition = rememberInfiniteTransition()
+        val angle by infiniteTransition.animateFloat(
+            initialValue = 0f, targetValue = 360f, animationSpec = infiniteRepeatable(
+                animation = tween(animationDuration, easing = LinearEasing),
+                repeatMode = RepeatMode.Restart
+            )
+        )
+        val brush = Brush.sweepGradient(gradient)
+        Modifier.drawWithContent {
+
+            val strokeWidthPx = strokeWith.toPx()
+            val width = size.width
+            val height = size.height
+
+            drawContent()
+
+            with(drawContext.canvas.nativeCanvas) {
+                val checkPoint = saveLayer(null, null)
+
+                // Destination
+                drawRect(
+                    color = Color.Gray,
+                    topLeft = Offset(strokeWidthPx / 2, strokeWidthPx / 2),
+                    size = Size(width - strokeWidthPx, height - strokeWidthPx),
+                    style = Stroke(strokeWidthPx)
+                )
+
+                // Source
+                rotate(angle) {
+
+                    drawCircle(
+                        brush = brush,
+                        radius = size.width,
+                        blendMode = BlendMode.SrcIn,
+                    )
+                }
+
+                restoreToCount(checkPoint)
+            }
+        }
+    } else return@composed this@animatedBorder
 }
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun CreditCard(
-    modifier: Modifier = Modifier,
-    flipped: Boolean = false
+    modifier: Modifier = Modifier
 ) {
     val rotateY = animateFloatAsState(
-        targetValue = if (flipped) 180f else 0f,
-        animationSpec = tween(
+        targetValue = if (CVVInput) 180f else 0f, animationSpec = tween(
             durationMillis = 500
         )
     )
-    BoxWithConstraints(
-        modifier = Modifier
-            .padding(7.5.dp)
-            .aspectRatio(1.586f)
-            .clip(RoundedCornerShape(10.dp))
-            .background(Color(0xFF280028))
-            .graphicsLayer {
-                rotationY = rotateY.value
-                rotationX = 0f
-                rotationZ = 0f
-            }
-    ) {
+    BoxWithConstraints(modifier = Modifier
+        .padding(7.5.dp)
+        .aspectRatio(1.586f)
+        .clip(RoundedCornerShape(10.dp))
+        .background(Color(0xFF280028))
+        .graphicsLayer {
+            rotationY = rotateY.value
+            rotationX = 0f
+            rotationZ = 0f
+        }) {
         val width = constraints.maxWidth
         val height = constraints.maxHeight
 
@@ -107,28 +151,20 @@ fun CreditCard(
             close()
         }
         Canvas(
-            modifier = Modifier
-                .fillMaxSize()
+            modifier = Modifier.fillMaxSize()
         ) {
             drawPath(
-                path = mediumColoredPath,
-                color = Color(0xC32E002E)
+                path = mediumColoredPath, color = Color(0xC32E002E)
             )
             drawPath(
-                path = lightColoredPath,
-                color = Color(0xFF33003F)
+                path = lightColoredPath, color = Color(0xFF33003F)
             )
         }
 
-        if (!flipped)
-            CardFront(
-                modifier = modifier,
-                year = 23,
-                month = 12,
-                cardHolderName = "mohammed samy"
-            )
-        else
-            CreditCardBack()
+        if (!CVVInput) CardFront(
+            modifier = modifier
+        )
+        else CreditCardBack()
 
     }
 
@@ -137,10 +173,6 @@ fun CreditCard(
 @Composable
 fun CardFront(
     modifier: Modifier,
-    cardNumber: String = "xxxx xxxx xxxx xxxx",
-    year: Int,
-    month: Int,
-    cardHolderName: String
 ) {
 
     Box(
@@ -150,6 +182,7 @@ fun CardFront(
             .clip(MaterialTheme.shapes.medium)
     ) {
         Row(
+
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
@@ -169,7 +202,10 @@ fun CardFront(
                 color = Color.White,
                 style = MaterialTheme.typography.h6,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .animatedBorder(2.dp, 2000, isVisible = numberInput)
+                    .padding(5.dp)
             )
         }
 
@@ -179,35 +215,45 @@ fun CardFront(
 
 
             Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
                     text = "CARDHOLDER NAME",
                     color = Color.White.copy(alpha = 0.6f),
                     style = MaterialTheme.typography.caption,
-                )
+
+                    )
                 Text(
                     text = "EXPIRATION DATE",
                     color = Color.White.copy(alpha = 0.6f),
                     style = MaterialTheme.typography.caption,
-                    textAlign = TextAlign.End
+                    textAlign = TextAlign.End,
                 )
             }
 
             Row(modifier = Modifier.fillMaxWidth()) {
                 Text(
-                    text = cardHolderName,
+                    text = cardholderName,
                     color = Color.White,
                     style = MaterialTheme.typography.body1,
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier
+                        .weight(1f)
+                        .animatedBorder(
+                            2.dp,
+                            2000,
+                            isVisible = nameInput
+                        )
                 )
                 Text(
-                    text = "ExDate",
+                    text = expirationDate,
                     color = Color.White,
                     style = MaterialTheme.typography.body1,
-                    textAlign = TextAlign.End
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.animatedBorder(
+                        2.dp,
+                        2000,
+                        isVisible = dateInput
+                    )
                 )
             }
         }
@@ -224,23 +270,23 @@ fun CardFront(
 
 @Composable
 fun CreditCardBack() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clip(MaterialTheme.shapes.medium)
-            .graphicsLayer {
-                rotationY = -180f
-                rotationX = 0f
-                rotationZ = 0f
-            }
-    ) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .clip(MaterialTheme.shapes.medium)
+        .graphicsLayer {
+            rotationY = -180f
+            rotationX = 0f
+            rotationZ = 0f
+        }) {
         Column(
             modifier = Modifier.fillMaxWidth()
         ) {
             Spacer(modifier = Modifier.height(40.dp))
-            Box(modifier = Modifier.fillMaxWidth()
-                .background(Color.Black)
-                .height(45.dp)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Black)
+                    .height(45.dp)
 
             )
 
@@ -258,7 +304,7 @@ fun CreditCardBack() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(45.dp)
-                    .padding(horizontal = 40.dp )
+                    .padding(horizontal = 40.dp)
                     .background(Color.White)
                     .padding(start = 3.dp)
 
@@ -280,80 +326,78 @@ fun CreditCardBack() {
 @Preview
 @Composable
 fun Card() {
-    var rotat by remember {
-        mutableStateOf(false)
-    }
     Column {
-        CreditCard(flipped = rotat)
-        Button(onClick = { rotat = !rotat }) {
-            Text(text = "flip")
-        }
+        CreditCard()
+
         var text by remember { mutableStateOf("") }
 
 
         PaymentInputForm()
+
+        Box(
+            modifier = Modifier
+                .size(400.dp, 70.dp)
+                .background(Color.Black)
+                .animatedBorder(1.dp, 500)
+        )
     }
 
 }
 
 @Composable
 fun PaymentInputForm() {
-    var cardholderName by remember { mutableStateOf("") }
-    var cardholderNameActive by remember { mutableStateOf(false) }
-    var cardNumber by remember { mutableStateOf("") }
-    var cardNumberActive by remember { mutableStateOf(false) }
-    var expirationDate by remember { mutableStateOf("") }
-    var expirationDateActive by remember { mutableStateOf(false) }
-    var cvv by remember { mutableStateOf("") }
-    var cvvActive by remember { mutableStateOf(false) }
-    if(cardNumberActive){
-        Toast.makeText(LocalContext.current , "focus" , Toast.LENGTH_LONG).show()
-    }
+
+
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
         // Cardholder Name
 
-        OutlinedTextField(
-            value = cardholderName,
-            onValueChange = {
-                cardholderName = it
-            },
-            label = { Text("Cardholder Name") }
-        )
-
         Spacer(modifier = Modifier.height(16.dp))
 
         // Card Number
-        OutlinedTextField(
-            value = cardNumber,
+        OutlinedTextField(value = cardNumber,
             onValueChange = { cardNumber = it },
             label = { Text("Card Number") },
-            visualTransformation = CardNumberMask("-"),
-            modifier = Modifier.onFocusChanged { cardNumberActive =  it.isFocused }
-        )
+            visualTransformation = CardNumberMask(),
+            modifier = Modifier.onFocusChanged { numberInput = it.isFocused })
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
+            OutlinedTextField(value = cardholderName, onValueChange = {
+                cardholderName = it
+            }, label = { Text("Cardholder Name") },
+                modifier = Modifier
+                    .weight(2f)
+                    .onFocusChanged { nameInput = it.isFocused }
+            )
+            Spacer(modifier = Modifier.width(16.dp))
             // Expiration Date
             OutlinedTextField(
-                value = "",
-                onValueChange = { },
-                label = { Text("Expiration Date") },
-                modifier = Modifier.weight(1f)
+                value = expirationDate,
+                onValueChange = { expirationDate = it },
+                visualTransformation = ExpirationDateMask(),
+                label = { Text("Date") },
+                modifier = Modifier
+                    .weight(1f)
+                    .onFocusChanged { dateInput = it.isFocused }
             )
 
             Spacer(modifier = Modifier.width(16.dp))
 
             // CVV
             OutlinedTextField(
-                value = "",
-                onValueChange = { },
+                value = cvv,
+                onValueChange = { cvv = it },
                 label = { Text("CVV") },
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .onFocusChanged {
+                        CVVInput = it.isFocused
+                    }
             )
         }
 
@@ -361,29 +405,13 @@ fun PaymentInputForm() {
 
         // Submit Button
         Button(
-            onClick = { /* Handle submit button click */ },
-            modifier = Modifier.align(Alignment.End)
+            onClick = { /* Handle submit button click */ }, modifier = Modifier.align(Alignment.End)
         ) {
             Text("Submit")
         }
     }
 }
 
-private fun formatCreditCardNumber(input: String): String {
-    // Remove any non-digit characters from the input
-    val digitsOnly = input.replace(Regex("[^\\d]"), "")
-
-    // Insert a space after every fourth character
-    val formattedText = StringBuilder()
-    for (i in digitsOnly.indices) {
-        if (i > 0 && i % 4 == 0) {
-            formattedText.append(" ")
-        }
-        formattedText.append(digitsOnly[i])
-    }
-
-    return formattedText.toString()
-}
 
 
 
